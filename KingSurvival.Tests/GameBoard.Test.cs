f3340de12640a73664f0f0b9eb906d21e8a22806
TeodorKurtev <teodor.ivanov92@gmail.com>
2013-05-12 23:00:23 +0300

    Added first Unit Tests.

---
+++ KingSurvival.Tests/GameBoard.Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KingSurvival.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GameBoard_CheckPositionTestNegativeNumbers()
+        {
+            GameBoard gameBoard = new GameBoard();
+
+            Assert.IsFalse(gameBoard.CheckPositionInBoard(new int[] { -10, -10 }));
+        }
+    }
+}
---
+++ KingSurvival.Tests/PawnA.Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KingSurvival.Tests
+{
+    [TestClass]
+    public class PawnATest
+    {
+        [TestMethod]
+        public void PawnA_TestPawnPosition()
+        {
+            PawnA pawnA = new PawnA();
+
+            int[] actual = { 2, 4 };
+
+            Assert.AreEqual(pawnA.FigurePosition[0], actual[0]);
+        }
+    }
+}
