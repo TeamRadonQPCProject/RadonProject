7f6bfa7fce7dc003e3259b8dc8f8fbfe39a80952
Galin Georgiev <galingeorgiev87@gmail.com>
2013-05-12 13:41:49 +0300

    Major changes in classes Figure, GameEngine and King. Logic for king is moved to class King. Method CheckInput is moved to Abstract class Figure because it is common for Pawn and King.

--- King-Survival-2/GameEngine.cs
+++ King-Survival-2/GameEngine.cs
@@ -48,7 +48,6 @@
         /* COMMAND PROCESS */
         public void InteractWithUser()
         {
-
             if (this.GameIsFinished)
             {
                 Console.WriteLine("Game is finished!");
@@ -58,8 +57,22 @@
             {
                 if (this.MovementsCounter % 2 == 0)
                 {
-                    this.ShowCurrentBoard();
-                    this.ProcessKingSide();
+                    int currentKingXAxe = theKing.FigurePosition[0];
+                    bool isKingExit = theKing.CheckForKingExit(currentKingXAxe);
+                    if (isKingExit)
+                    {
+                        Console.WriteLine("End!");
+                        Console.WriteLine("King wins in {0} moves!", this.MovementsCounter / 2);
+                        this.GameIsFinished = true;
+                    }
+
+                    if (GameIsFinished != true)
+                    {
+                        this.ShowCurrentBoard();
+                        theKing.ProcessKingSide(kingSurvivalGameBoard);
+                        this.MovementsCounter++;
+                        InteractWithUser();
+                    }
                 }
                 else
                 {
@@ -92,32 +105,25 @@
         public bool CheckPlayerInput(string stringToCheck)
         {
             char startLetter = stringToCheck[0];
-            if (this.MovementsCounter % 2 == 0) // King turn
+            switch (startLetter)
             {
-                return this.ChechInput(stringToCheck, theKing.ValidFigureInputs);
-            }
-            else // PawnsTurn
-            {
-                switch (startLetter)
-                {
-                    case 'A':
-                        return ChechInput(stringToCheck, firstPawn.ValidFigureInputs);
+                case 'A':
+                    return ChechInput(stringToCheck, firstPawn.ValidFigureInputs);
 
-                    case 'B':
-                        return ChechInput(stringToCheck, secondPawn.ValidFigureInputs);
+                case 'B':
+                    return ChechInput(stringToCheck, secondPawn.ValidFigureInputs);
 
-                    case 'C':
-                        return ChechInput(stringToCheck, thirdPawn.ValidFigureInputs);
+                case 'C':
+                    return ChechInput(stringToCheck, thirdPawn.ValidFigureInputs);
 
-                    case 'D':
-                        return ChechInput(stringToCheck, fourthPawn.ValidFigureInputs);
+                case 'D':
+                    return ChechInput(stringToCheck, fourthPawn.ValidFigureInputs);
 
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Invalid command name!");
-                        Console.ResetColor();
-                        return false;
-                }
+                default:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid command name!");
+                    Console.ResetColor();
+                    return false;
             }
         }
 
@@ -151,7 +157,7 @@
                 }
             }
 
-            // write on console if There is no Valid input
+            // write on console if there is no valid input
             if (!hasAnEqual)
             {
                 Console.BackgroundColor = ConsoleColor.Red;
@@ -195,188 +201,6 @@
 
         /* KING LOGIC */
 
-        public void ProcessKingSide()
-        {
-            bool isExecuted = false;
-            while (!isExecuted)
-            {
-                Console.BackgroundColor = ConsoleColor.DarkGreen;
-                Console.Write("Please enter king's turn: ");
-                Console.ResetColor();
-                string input = ReadKingInput();
-
-                if (string.IsNullOrEmpty(input))
-                {
-                    isExecuted = false;
-                    Console.BackgroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("Please enter something!");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    input = input.ToUpper();
-                    isExecuted = ProcessKingCommand(input);
-                }
-            }
-
-            InteractWithUser();
-        }
-
-        private bool ProcessKingCommand(string checkedInput)
-        {
-            bool isCommandNameCorrect = CheckPlayerInput(checkedInput);
-
-            if (isCommandNameCorrect)
-            {
-                if (checkedInput[1] == 'U')
-                {
-                    if (checkedInput[2] == 'L')
-                    {
-                        MoveKing('U', 'L');
-                    }
-                    else
-                    {
-                        MoveKing('U', 'R');
-                    }
-
-                    return true;
-                }
-                else
-                {
-                    if (checkedInput[2] == 'L')
-                    {
-                        MoveKing('D', 'L');
-                    }
-                    else
-                    {
-                        MoveKing('D', 'R');
-                    }
-
-                    return true;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private void MoveKing(char firstDirection, char secondDirection)
-        {
-            int[] oldCoordinates = new int[2];
-
-            oldCoordinates[0] = theKing.FigurePosition[0];
-            oldCoordinates[1] = theKing.FigurePosition[1];
-
-            int[] newCoords = new int[2];
-            newCoords = CheckNextKingPosition(oldCoordinates, firstDirection, secondDirection);
-
-            if (newCoords != null)
-            {
-                theKing.FigurePosition[0] = newCoords[0];
-                theKing.FigurePosition[1] = newCoords[1];
-            }
-        }
-
-        public int[] CheckNextKingPosition(int[] currentCoordinates, char firstDirection, char secondDirection)
-        {
-            int[] displacementDownLeft = { 1, -2 };
-            int[] displacementDownRight = { 1, 2 };
-            int[] displacementUpLeft = { -1, -2 };
-            int[] displacementUpRight = { -1, 2 };
-
-            string direction = firstDirection.ToString() + secondDirection.ToString();
-            switch (direction)
-            {
-                case "UL":
-                    return CheckKingAvailableMove(currentCoordinates, displacementUpLeft);
-                case "UR":
-                    return CheckKingAvailableMove(currentCoordinates, displacementUpRight);
-                case "DL":
-                    return CheckKingAvailableMove(currentCoordinates, displacementDownLeft);
-                case "DR":
-                    return CheckKingAvailableMove(currentCoordinates, displacementDownRight);
-                default:
-                    return null;
-            }
-        }
-
-        private int[] CheckKingAvailableMove(int[] currentCoordinates, int[] displacementDirection)
-        {
-            int[] newCoords = new int[2];
-            newCoords[0] = currentCoordinates[0] + displacementDirection[0];
-            newCoords[1] = currentCoordinates[1] + displacementDirection[1];
-            if (kingSurvivalGameBoard.CheckPositionInBoard(newCoords) && kingSurvivalGameBoard.Board[newCoords[0], newCoords[1]] == ' ')
-            {
-                char sign = kingSurvivalGameBoard.Board[currentCoordinates[0], currentCoordinates[1]];
-                kingSurvivalGameBoard.Board[currentCoordinates[0], currentCoordinates[1]] = ' ';
-                kingSurvivalGameBoard.Board[newCoords[0], newCoords[1]] = sign;
-                this.MovementsCounter++;
-
-                for (int i = 0; i < 4; i++)
-                {
-                    theKing.FigureExistingMoves[i] = true;
-                }
-
-                CheckForKingExit(newCoords[0]);
-                return newCoords;
-            }
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine("You can't go in this direction! ");
-                Console.ResetColor();
-                return null;
-            }
-        }
-
-        private void HasKingExistingMove(int[] currentCoords)
-        {
-            int[] newCoords = new int[2];
-            int[,] displacements = 
-            {
-                { 1, -2 },
-                { 1, 2 },
-                { -1, -2 },
-                { -1, 2 },
-            };
-
-            for (int i = 0; i < displacements.GetLength(0); i++)
-            {
-                newCoords[0] = currentCoords[0] + displacements[i, 0];
-                newCoords[1] = currentCoords[1] + displacements[i, 1];
-                if (!(kingSurvivalGameBoard.CheckPositionInBoard(newCoords) && kingSurvivalGameBoard.Board[newCoords[0], newCoords[1]] == ' '))
-                {
-                    theKing.FigureExistingMoves[i] = false;
-                }
-            }
-
-            bool allAreFalse = true;
-            for (int i = 0; i < 4; i++)
-            {
-                if (theKing.FigureExistingMoves[i] == true)
-                {
-                    allAreFalse = false;
-                }
-            }
-
-            if (allAreFalse)
-            {
-                this.GameIsFinished = true;
-                Console.WriteLine("King loses!");
-            }
-        }
-
-        public void CheckForKingExit(int currentKingXAxe)
-        {
-            if (currentKingXAxe == 2)
-            {
-                Console.WriteLine("End!");
-                Console.WriteLine("King wins in {0} moves!", this.MovementsCounter / 2);
-                this.GameIsFinished = true;
-            }
-        }
-
         /* PAWN LOGIC */
 
         public void ProcessPawnSide()
@@ -403,7 +227,13 @@
                 }
             }
 
-            HasKingExistingMove(theKing.FigurePosition);
+            bool hasKingExistingMove = theKing.HasKingExistingMove(theKing.FigurePosition, kingSurvivalGameBoard);
+            if (hasKingExistingMove == false)
+            {
+                this.GameIsFinished = true;
+                Console.WriteLine("King loses!");
+            }
+            
             InteractWithUser();
         }
 
@@ -515,80 +345,29 @@
 
         // TODO: Move to King class
 
-        private int indexKing = 0;
-        public virtual string ReadKingInput()
-        {
-            string[] sampleInput = new string[] {
-            "kur",
-            "kur",
-            "kul",
-            "kdr",
-            "kdr",
-            "kdr"
-            };
-
-            // Test all pawns down
-            //string[] sampleInput = new string[] {
-            //"kur",
-            //"kul",
-            //"kur",
-            //"kul",
-            //"kur",
-            //"kul",
-            //"kur",
-            //"kul",
-            //"kur",
-            //"kur",
-            //"kur",
-            //"kdr",
-            //"kdr",
-            //"kdr",
-            //"kul",
-            //"kul",
-            //"kdr",
-            //"kul",
-            //"kdr",
-            //"kul",
-            //"kdr",
-            //"kul",
-            //"kdr",
-            //"kul",
-            //"kdr",
-            //"kul",
-            //"kdr",
-            //"kul",
-            //"kul",
-            //"kul",
-            //"kdr",
-            //"kul",
-            //"kdr",
-            //"kul",
-            //"kdr",
-            //"kdl"
-            //};
-
-            if (indexKing <= sampleInput.Length)
-            {
-                //Console.WriteLine(indexKing);
-                Console.WriteLine(sampleInput[indexKing]);
-                return sampleInput[indexKing++];
-            }
-            
-            string kingInput = Console.ReadLine();
-            return kingInput;
-        }
-
         private int indexPawn = 0;
+
         public virtual string ReadPawnInput()
         {
-            string[] sampleInput = new string[] {
-            "cdr",
-            "cdr",
-            "cdl",
-            "cdr",
-            "cdr",
-            "cdl"
-            };
+            //string[] sampleInput = new string[] {
+            //"cdr",
+            //"cdr",
+            //"cdl",
+            //"cdr",
+            //"cdr",
+            //"cdl"
+            //};
+
+            //string[] sampleInput = new string[]
+            //{
+            //    "ddl",
+            //    "ddl",
+            //    "ddl",
+            //    "ddl",
+            //    "ddl",
+            //    "ddl",
+            //    "ddr"
+            //};
 
             // Test all pawns down
             //string[] sampleInput = new string[] {
@@ -628,12 +407,13 @@
             //"ddl"
             //};
 
-            if (indexPawn < sampleInput.Length)
-            {
-                //Console.WriteLine(indexPawn);
-                Console.WriteLine(sampleInput[indexPawn]);
-                return sampleInput[indexPawn++];
-            }
+            //if (indexPawn < sampleInput.Length)
+            //{
+            //    //Console.WriteLine(indexPawn);
+            //    Console.WriteLine(sampleInput[indexPawn]);
+            //    return sampleInput[indexPawn++];
+            //}
+
             string pawnInput = Console.ReadLine();
             return pawnInput;
         }
--- King-Survival-2/King.cs
+++ King-Survival-2/King.cs
@@ -74,5 +74,273 @@
         {
             throw new NotImplementedException();
         }
+
+
+        public void ProcessKingSide(GameBoard currentBoard)
+        {
+            bool isExecuted = false;
+            while (!isExecuted)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.Write("Please enter king's turn: ");
+                Console.ResetColor();
+                string input = ReadKingInput();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    isExecuted = false;
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Please enter something!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    input = input.ToUpper();
+                    isExecuted = ProcessKingCommand(input, currentBoard);
+                }
+            }
+        }
+
+        public bool HasKingExistingMove(int[] currentCoords, GameBoard currentBoard)
+        {
+            int[] newCoords = new int[2];
+            int[,] displacements = 
+            {
+                { 1, -2 },
+                { 1, 2 },
+                { -1, -2 },
+                { -1, 2 },
+            };
+
+            for (int i = 0; i < displacements.GetLength(0); i++)
+            {
+                newCoords[0] = currentCoords[0] + displacements[i, 0];
+                newCoords[1] = currentCoords[1] + displacements[i, 1];
+                if (!(currentBoard.CheckPositionInBoard(newCoords) && currentBoard.Board[newCoords[0], newCoords[1]] == ' '))
+                {
+                    this.FigureExistingMoves[i] = false;
+                }
+            }
+
+            bool allAreFalse = true;
+            for (int i = 0; i < 4; i++)
+            {
+                if (this.FigureExistingMoves[i] == true)
+                {
+                    allAreFalse = false;
+                }
+            }
+
+            if (allAreFalse)
+            {
+                return false;
+                //this.GameIsFinished = true;
+                //Console.WriteLine("King loses!");
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool CheckForKingExit(int currentKingXAxe)
+        {
+            if (currentKingXAxe == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool ProcessKingCommand(string checkedInput, GameBoard currentBoard)
+        {
+            bool isCommandNameCorrect = base.ChechInput(checkedInput, this.ValidFigureInputs);
+
+            if (isCommandNameCorrect)
+            {
+                if (checkedInput[1] == 'U')
+                {
+                    if (checkedInput[2] == 'L')
+                    {
+                        MoveKing('U', 'L', currentBoard);
+                    }
+                    else
+                    {
+                        MoveKing('U', 'R', currentBoard);
+                    }
+
+                    return true;
+                }
+                else
+                {
+                    if (checkedInput[2] == 'L')
+                    {
+                        MoveKing('D', 'L', currentBoard);
+                    }
+                    else
+                    {
+                        MoveKing('D', 'R', currentBoard);
+                    }
+
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void MoveKing(char firstDirection, char secondDirection, GameBoard currentBoard)
+        {
+            int[] oldCoordinates = new int[2];
+
+            oldCoordinates[0] = this.FigurePosition[0];
+            oldCoordinates[1] = this.FigurePosition[1];
+
+            int[] newCoords = new int[2];
+            newCoords = CheckNextKingPosition(oldCoordinates, firstDirection, secondDirection, currentBoard);
+
+            if (newCoords != null)
+            {
+                this.FigurePosition[0] = newCoords[0];
+                this.FigurePosition[1] = newCoords[1];
+            }
+            else
+            {
+                ProcessKingSide(currentBoard);
+            }
+        }
+
+        private int[] CheckNextKingPosition(int[] currentCoordinates, char firstDirection, char secondDirection, GameBoard currentBoard)
+        {
+            int[] displacementDownLeft = { 1, -2 };
+            int[] displacementDownRight = { 1, 2 };
+            int[] displacementUpLeft = { -1, -2 };
+            int[] displacementUpRight = { -1, 2 };
+
+            string direction = firstDirection.ToString() + secondDirection.ToString();
+            switch (direction)
+            {
+                case "UL":
+                    return CheckKingAvailableMove(currentCoordinates, displacementUpLeft, currentBoard);
+                case "UR":
+                    return CheckKingAvailableMove(currentCoordinates, displacementUpRight, currentBoard);
+                case "DL":
+                    return CheckKingAvailableMove(currentCoordinates, displacementDownLeft, currentBoard);
+                case "DR":
+                    return CheckKingAvailableMove(currentCoordinates, displacementDownRight, currentBoard);
+                default:
+                    return null;
+            }
+        }
+
+        private int[] CheckKingAvailableMove(int[] currentCoordinates, int[] displacementDirection, GameBoard currentBoard)
+        {
+            int[] newCoords = new int[2];
+            newCoords[0] = currentCoordinates[0] + displacementDirection[0];
+            newCoords[1] = currentCoordinates[1] + displacementDirection[1];
+            if (currentBoard.CheckPositionInBoard(newCoords) && currentBoard.Board[newCoords[0], newCoords[1]] == ' ')
+            {
+                return MoveKingOnBoard(currentCoordinates, newCoords, currentBoard);
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("You can't go in this direction! ");
+                Console.ResetColor();
+                return null;
+            }
+        }
+
+        private int[] MoveKingOnBoard(int[] currentCoordinates, int[] newCoords, GameBoard currentBoard)
+        {
+            char sign = currentBoard.Board[currentCoordinates[0], currentCoordinates[1]];
+            currentBoard.Board[currentCoordinates[0], currentCoordinates[1]] = ' ';
+            currentBoard.Board[newCoords[0], newCoords[1]] = sign;
+
+            for (int i = 0; i < 4; i++)
+            {
+                this.FigureExistingMoves[i] = true;
+            }
+
+            return newCoords;
+        }
+
+        private int indexKing = 0;
+        public virtual string ReadKingInput()
+        {
+            //string[] sampleInput = new string[] {
+            //"kur",
+            //"kur",
+            //"kul",
+            //"kdr",
+            //"kdr",
+            //"kdr"
+            //};
+
+            //string[] sampleInput = new string[] {
+            //"kur",
+            //"kur",
+            //"kur",
+            //"kur",
+            //"kul",
+            //"kul",
+            //"kur"
+            //};
+
+            // Test all pawns down
+            //string[] sampleInput = new string[] {
+            //"kur",
+            //"kul",
+            //"kur",
+            //"kul",
+            //"kur",
+            //"kul",
+            //"kur",
+            //"kul",
+            //"kur",
+            //"kur",
+            //"kur",
+            //"kdr",
+            //"kdr",
+            //"kdr",
+            //"kul",
+            //"kul",
+            //"kdr",
+            //"kul",
+            //"kdr",
+            //"kul",
+            //"kdr",
+            //"kul",
+            //"kdr",
+            //"kul",
+            //"kdr",
+            //"kul",
+            //"kdr",
+            //"kul",
+            //"kul",
+            //"kul",
+            //"kdr",
+            //"kul",
+            //"kdr",
+            //"kul",
+            //"kdr",
+            //"kdl"
+            //};
+
+            //if (indexKing < sampleInput.Length)
+            //{
+            //    //Console.WriteLine(indexKing);
+            //    Console.WriteLine(sampleInput[indexKing]);
+            //    return sampleInput[indexKing++];
+            //}
+
+            string kingInput = Console.ReadLine();
+            return kingInput;
+        }
     }
 }
